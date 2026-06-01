524dc7396b10b8801bab7e29493556da870b327a
Fourier <52960367+245516766@users.noreply.github.com>
2026-06-01 20:41:53 +0800

    fix: add another func name (#358)

--- third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
@@ -41,6 +41,7 @@ using namespace triton;
 namespace {
 
 static constexpr llvm::StringLiteral interceptrFunc[] {
+  "chunk_transform_qk_fwd_kernel",
   "_attn_fwd",
   "_attn_bwd",
   "_kernel_matmul_fp8_row_non_persistent",
